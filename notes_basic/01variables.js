06e67774dbfd931a73955b039308411f23c92092
Sudhakarsah01 <137956052+Sudhakarsah01@users.noreply.github.com>
2025-04-10 10:06:59 +0000

    container config done

---
+++ notes_basic/01variables.js
@@ -0,0 +1,7 @@
+const accountId =1234
+let Name = "Sudhakar"
+var accountEmail="sudhakarsah.npl@gmail.com"
+let accountPassword="123Sudh"
+
+console.log(Name);
+console.table([accountId, accountEmail, Name, accountPassword])
