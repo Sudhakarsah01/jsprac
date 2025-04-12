facbbd7db5f2073849dfe416113484b0dbffe3da
Sudhakarsah01 <137956052+Sudhakarsah01@users.noreply.github.com>
2025-04-12 06:07:15 +0000

    conversion done

---
+++ notes_basic/conversion_dt.js
@@ -0,0 +1,15 @@
+let number = 10
+let convTostring = String(number)
+
+// console.log(typeof (convTostring));
+// console.log(convTostring);
+
+let tostring = "10"
+let convoTonum = Number(tostring)
+
+// console.log(typeof(convoTonum));
+// console.log(convoTonum);
+
+
+
+
